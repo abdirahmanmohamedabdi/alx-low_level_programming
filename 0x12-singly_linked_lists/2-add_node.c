01bd10f3752330e67c6ee1bad9bea2478f9f1fa5
abdirahmanmohamedabdi <abdirahmanabdi.ma@gmail.com>
2024-02-25 06:31:57 -0800

    new

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * createNewNode - creates new node
+ * @str: string
+ * Return: node
+ */
+list_t *createNewNode(const char *str)
+{
+list_t *newNode = (list_t *)malloc(sizeof(list_t));
+if (newNode == NULL)
+return (NULL);
+newNode->str = strdup(str);
+newNode->len = strlen(str);
+newNode->next= NULL;
+return (newNode);
+}
+/**
+ * add_node - creates a new node
+ * @head: pointer
+ * @str: string
+ * Return: A pointer
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *newNode = createNewNode(str);
+if (newNode == NULL)
+return (NULL);
+newNode->next = *head;
+*head = newNode;
+return (newNode);
+}
