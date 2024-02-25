a3d263e411441b6221aa53fad2aff4f99c77f8f7
abdirahmanmohamedabdi <abdirahmanabdi.ma@gmail.com>
2024-02-25 05:55:05 -0800

    new

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -38,12 +38,3 @@ int main(void)
     free(new);
     return (0);
 }
-julien@ubuntu:~/0x12. Singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_list.c -o a
-julien@ubuntu:~/0x12. Singly linked lists$ ./a 
-[5] Hello
-[5] World
--> 2 elements
-
-[0] (nil)
-[5] World
--> 2 elements
